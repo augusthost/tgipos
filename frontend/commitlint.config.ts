78eaef31c1df6c2bf107228ee62793f999213075
Ronald Aug <saisenfa@gmail.com>
2025-05-10 23:37:07 +0630

    fix: fix husky issue

--- frontend/commitlint.config.ts
+++ frontend/commitlint.config.ts
@@ -1,7 +1,14 @@
-import type { UserConfig } from "@commitlint/types";
+import type { UserConfig } from '@commitlint/types';
 
-const config: UserConfig = {
-  extends: ["@commitlint/config-conventional"],
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    ],
+  },
 };
 
-export default config;
+export default Configuration;
